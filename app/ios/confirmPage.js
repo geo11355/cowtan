ca52e5429924530ce4866174e589432f2dc02c09
Phil <philphoo95@gmail.com>
2016-01-31 17:11:42 -0500

    confirm page styling

--- app/ios/confirmPage.js
+++ app/ios/confirmPage.js
@@ -32,19 +32,27 @@ class ConfirmPage extends Component{
 
 		
 		return(
-
-			<View style = {styles.container}>
-				<ScrollView contentContainerStyle = {styles.scroll}>
-					{/*<View style = {styles.showroomBanner}>
-						<Text style = {styles.location}>Welcome to the {this.props.location.city} Showroom</Text>
-					</View>	*/}
-					<View style = {styles.titleRow}>
-						<Text style = {styles.description}>
-							Welcome to the {this.props.location.city} Showroom
-						</Text>
+			<ScrollView contentContainerStyle = {styles.scroll}>
+				{/*<View style = {styles.showroomBanner}>
+					<Text style = {styles.location}>Welcome to the {this.props.location.city} Showroom</Text>
+				</View>	*/}
+				<View style = {styles.topContainer}>
+					<Text style = {styles.description}>
+						Welcome to the 
+					</Text>
+				
+					<View style = {styles.circle}>
+						<Text style = {styles.showroomText}>{this.props.location.city}</Text>
+						<Image style = {styles.image}
+                    		   source = {require('./../media/location.png')}/>
 					</View>
 
-					<View style = {styles.outerTextbox}>
+					<Text style = {styles.description}>Showroom!</Text>
+				</View>
+
+				<View style = {styles.lowerBox}>
+					<Text style = {styles.confirmText}>Your Information:</Text>
+					<View>
 						<View style = {styles.row}>
 							<View style = {styles.rowPlaceholder}></View>
 							<View style = {styles.categoryBox}><Text style = {styles.categoryText}>Company: </Text></View>
@@ -63,11 +71,11 @@ class ConfirmPage extends Component{
 
 						<View style = {styles.row}>
 							<View style = {styles.rowPlaceholder}></View>
-							<View style = {styles.categoryBox}><Text style = {styles.categoryText}>Phone Number: </Text></View>
+							<View style = {styles.categoryBox}><Text style = {styles.categoryText}>Phone: </Text></View>
 							<View style = {styles.userBox}><Text style = {styles.userText}>{this.props.user.phone}</Text></View>
 						</View>
 					</View>
-				</ScrollView>
+				</View>
 
 				<View style = {styles.buttonsContainer}>
 					<TouchableHighlight
@@ -77,12 +85,12 @@ class ConfirmPage extends Component{
 						<Text style = {styles.buttonText}>Continue</Text>
 					</TouchableHighlight>
 					{/*<TouchableHighlight
-						style = {styles.notYouButton}
+						style = s{styles.notYouButton}
 						underlayColor = 'transparent'>
 						<Text style = {styles.notYouText} underlayColor = 'transparent'>Not You?</Text>
 					</TouchableHighlight>*/}
 				</View>
-			</View>
+			</ScrollView>
 		);
 	}
 
@@ -107,11 +115,11 @@ var styles = StyleSheet.create({
 		flex: .8,
 	},
 	scroll: {
-		justifyContent: 'center',
+		//justifyContent: 'center',
 		flex: 1,
 		//borderBottomWidth: 1.5,
 		//borderColor: '#b9b6b6',
-		marginBottom: 15,
+		//marginBottom: 15,
 	},
 	showroomBanner: {
 		backgroundColor: '#f0eeee',
@@ -128,6 +136,20 @@ var styles = StyleSheet.create({
         },
         //borderWidth: 1,
         //borderColor: '#b9b6b6'
+	},
+	lowerBox: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		//alignItems: 'center'
+		//borderWidth: 1
+		shadowRadius: 4,
+        shadowOpacity: 0.8,
+        shadowColor: 'gray',
+        shadowOffset: {
+            width: 2,
+           	height:2,
+        }
 	},
 	row: {
 		flexDirection: 'row',
@@ -146,11 +168,11 @@ var styles = StyleSheet.create({
 		marginTop: 10
 	},
 	description: {
-		fontSize: 22,
+		fontSize: 20,
 		fontWeight: 'bold',
 		alignSelf: 'center',
 		marginBottom: 10,
-		marginTop: 15,
+		marginTop: 10,
 	},
 	categoryText: {
 		fontSize: 16,
@@ -206,18 +228,57 @@ var styles = StyleSheet.create({
 		marginBottom: 10,
 		color: 'black',
 		alignSelf: 'center',
-		fontSize: 18
+		fontSize: 16
 	},
-	titleRow: {
-	    flexDirection: 'row',
-	    marginTop: 5,
-	    borderBottomWidth: 1.5,
+	topContainer: {
+	    //flexDirection: 'row',
+	    marginTop: 0,
+	    //marginBottom: 5,
+	    borderBottomWidth: 1,
+	    backgroundColor: '#fcfbfb',
+	    borderBottomColor: '#d2d0d0',
 	    justifyContent: 'center',
+	    alignItems: 'center',
+	    shadowRadius: 4,
+        shadowOpacity: 0.8,
+        shadowColor: 'gray',
+        shadowOffset: {
+            width: 2,
+            height: 2
+        }
+
 	},
-	outerTextbox: {
-		//flex: 1,
-		marginTop: 30,
-	}
+	circle: {
+		height: 150,
+		width: 150,
+		borderRadius: 75,
+		borderWidth: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: '#800000',
+		//opacity: 0.8
+	},
+	showroomText: {
+		color: 'white',
+		fontSize: 16,
+		fontWeight: 'bold',
+		marginTop: 10
+	},
+	image: {
+		alignSelf: 'center',
+		resizeMode: 'contain',
+		height: 60,
+		width: 60,
+		marginTop: 5
+	},
+	confirmText: {
+		alignSelf: 'center',
+		marginBottom: 10,
+		fontSize: 18,
+		fontWeight: 'bold',
+		marginRight: 15,
+		marginLeft: 15
+	},
 
 });
 
